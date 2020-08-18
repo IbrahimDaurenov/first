82f078fcffbb9aa537df222cda782b78eed4c41d
IbrahimDaurenov <68897218+IbrahimDaurenov@users.noreply.github.com>
2020-08-19 01:25:24 +0600

    Create admin.py

---
+++ upgrade/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Comment)
+admin.site.register(Game)
+admin.site.register(Games)
+admin.site.register(Contact)
+# Register your models here.
