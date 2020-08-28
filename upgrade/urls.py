ddb75884927c80fb314562603755db1d76b3ac30
IbrahimDaurenov <68897218+IbrahimDaurenov@users.noreply.github.com>
2020-08-28 12:50:17 +0600

    Update urls.py

--- upgrade/urls.py
+++ upgrade/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name = 'animal_index'),
-
+    path('',views.index, name = 'komp_index'),
+    path('upgrade/',views.upgrade, name = 'komp_upgrade'),
+    path('site/', views.site, name = 'site'),
+    path('types/',views.types, name = 'types'),
+    path('game/', views.game, name = 'game'),
+    path('games/', views.games, name = 'games'),
     ]
