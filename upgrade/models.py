f2e44154afdba4642cec10cdc6799c8536d479a0
IbrahimDaurenov <68897218+IbrahimDaurenov@users.noreply.github.com>
2020-08-28 12:49:17 +0600

    Update models.py

--- upgrade/models.py
+++ upgrade/models.py
@@ -7,17 +7,30 @@ class Comment(models.Model):
     def __str__(self):
         return (self.author + ': ' + self.content)
 
-class Game(models.Model):
+class Type(models.Model):
     name = models.CharField(max_length = 64)
 
     def __str__(self):
         return(self.name)
 
-class Games(models.Model):
-    type =  models.ManyToManyField(Game, blank = True)
+class Nameofgame(models.Model):
+    name = models.CharField(max_length = 64)
+
+    def __str__(self):
+        return(self.name)
+
+class Game(models.Model):
+    name = models.ManyToManyField(Nameofgame, blank = True)
+    type =  models.ManyToManyField(Type, blank = True)
     comments = models.ManyToManyField(Comment, blank = True)
+
+    def __str__(self):
+        return (str(self.type) + ' ' + str(self.name))
 
 class Contact(models.Model):
     phone = models.CharField(max_length = 64)
+
+    def __str__(self):
+        return(self.phone)
 
 # Create your models here.
